4aa0775045e060cc54f90c8dd15eda8be08d594f
AndrazS4 <as84819@student.uni-lj.si>
2023-02-26 19:18:54 +0100

    Dodaj izracun  pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,9 @@ public class Gravitacija {
 	public static void main(String[] args) {
 		System.out.println("OIS je zakon!");
 	}
+	
+	public static double gravitacijskiPospesek(double nadmorskaVisina) {
+		//vrne pospesek po formuli a = G * M / (Rz + h)^2
+		return 6.674e-11d * 5.972e24d / ((6.371e6 + nadmorskaVisina)*(6.371e6 + nadmorskaVisina));
+	}
 }
